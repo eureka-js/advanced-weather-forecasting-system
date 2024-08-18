d8e59eebb13f4bda82431a7fcbdbcc14db36a187
eureka-js <jurstanic@gmail.com>
2024-08-18 20:35:15 +0200

    Add validation annotations to the WeatherForecastDTO class
    - Renamed UVIndex field to uvIndex in WeatherForecastDTO to follow Java naming conventions

--- Back-end/Spring-boot/awfs/src/main/java/en/weatherforecastapp/models/dto/WeatherForecastDTO.java
+++ Back-end/Spring-boot/awfs/src/main/java/en/weatherforecastapp/models/dto/WeatherForecastDTO.java
@@ -1,5 +1,8 @@
 package en.weatherforecastapp.models.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,10 +12,18 @@ import java.sql.Timestamp;
 @Data
 @AllArgsConstructor
 public class WeatherForecastDTO {
+    @NotBlank
     private String city;
+    @NotNull
     private Float temperature;
+    @NotNull
     private String description;
+    @NotNull
     private Timestamp dateTime;
-    private Integer UVIndex;
+    @NotNull
+    @Min(value = 0, message = "UV index must not be a negative integer")
+    private Integer uvIndex;
+    @NotNull
+    @Min(value = 0, message = "Visibility must not be a negative integer")
     private Integer visibility;
 }
