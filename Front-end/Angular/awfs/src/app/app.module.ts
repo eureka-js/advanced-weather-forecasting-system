b8a5e4e7bca5f39ce262d0e7b27b8408c6db5672
eureka-js <jurstanic@gmail.com>
2024-08-26 20:40:15 +0200

    Implement a navbar component in Angular
    - Implemented a navbar component with two routes: home and favorite
    - Made some minor adjustments to CSS across the site

--- Front-end/Angular/awfs/src/app/app.module.ts
+++ Front-end/Angular/awfs/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {provideHttpClient} from "@angular/common/http";
+import {CoreModule} from "./core/core.module";
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import {provideHttpClient} from "@angular/common/http";
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CoreModule
   ],
   providers: [provideHttpClient()],
   bootstrap: [AppComponent]
--- Front-end/Angular/awfs/src/app/core/core.module.ts
+++ Front-end/Angular/awfs/src/app/core/core.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import {RouterLink} from "@angular/router";
 
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule]
+  declarations: [
+    NavbarComponent
+  ],
+  exports: [
+    NavbarComponent
+  ],
+  imports: [CommonModule, RouterLink]
 })
 export class CoreModule { }
